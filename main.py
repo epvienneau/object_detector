0bf6b008ab5967ab9ba847ebec3b9fe6597c0ddf
epvienneau <emelina@vienneau.io>
2018-09-12 14:44:34 -0500

    training and validation work now!

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ def test(args, model, device, test_loader):
             output = model(data)
             criterion = nn.MSELoss()
             test_loss += criterion(output, target).item() 
-            pred = output.max(1, keepdim=True)[1] 
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            #pred = output.max(1, keepdim=True)[1]
+            correct += output.eq(target.view_as(output)).sum().item()
 
     test_loss /= len(test_loader.dataset)
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
@@ -46,12 +46,12 @@ def test(args, model, device, test_loader):
 
 def main():
     parser = argparse.ArgumentParser(description='PyTorch Object Detection Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--batch-size', type=int, default=1, metavar='N',
+                        help='input batch size for training (default: 1)')
+    parser.add_argument('--test-batch-size', type=int, default=1, metavar='N',
+                        help='input batch size for testing (default: 1)')
+    parser.add_argument('--epochs', type=int, default=20, metavar='N',
+                        help='number of epochs to train (default: 20)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                         help='learning rate (default: 0.01)')
     parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
