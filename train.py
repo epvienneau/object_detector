0bc9abea3f6ea57bf64ed943717148020cc2edda
epvienneau <epv4@duke.edu>
2018-09-12 23:58:40 -0500

    save losses to csv

--- train.py
+++ train.py
@@ -10,12 +10,17 @@ from img_loader import img_loader
 import torch.utils.model_zoo as model_zoo
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+training_loss = []
+validation_loss = []
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target= data.to(device), target.to(device)
-        optimizer.zero_grad()
+        optimizer.zero_grad() 
         output = model(data)
         criterion = nn.MSELoss()
         loss = criterion(output, target)
@@ -25,7 +30,8 @@ def train(args, model, device, train_loader, optimizer, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-
+        training_loss.append(loss.item())
+  
 def test(args, model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -40,6 +46,7 @@ def test(args, model, device, test_loader):
             correct += output.eq(target.view_as(output)).sum().item()
 
     test_loss /= len(test_loader.dataset)
+    validation_loss.append(test_loss)
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
@@ -95,9 +102,9 @@ def main():
                 coord1_val.append(float(line[1]))
                 coord2_val.append(float(line[2]))
     data_train = [coord1_train, coord2_train, img_file_train, img_path_train]
-    data_val = [coord1_val, coord2_val, img_file_val, img_path_val]
-    train_loader = torch.utils.data.DataLoader(img_loader(data_train), batch_size=1, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(img_loader(data_val), batch_size=1, shuffle=True, **kwargs)
+    data_val = [coord1_val, coord2_val, img_file_val, img_path_val] 
+    train_loader = torch.utils.data.DataLoader(img_loader(data_train), batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(img_loader(data_val), batch_size=args.batch_size, shuffle=True, **kwargs) 
 
     model = models.resnet18(pretrained=True, **kwargs).to(device)
     num_ftrs = model.fc.in_features
@@ -109,8 +116,22 @@ def main():
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
 
+    #epoch_axis = range(args.epochs)
+    #plt.plot(epoch_axis, training_loss, 'r', epoch_axis, validation_loss, 'b')
+    #plt.show()
     torch.save(model.state_dict(), './Resnetmodel.pt')
-    import pytest; pytest.set_trace()
+    with open('loss.csv', 'w', newline='') as csvfile:
+        losswriter = csv.writer(csvfile, delimiter=' ', 
+                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        losswriter.writerow('training')
+        print(len(training_loss))
+        print(len(validation_loss))
+        for item in training_loss:
+            losswriter.writerow(str(round(item, 4)))
+        losswriter.writerow('validation')
+        for item in validation_loss:
+            losswriter.writerow(str(round(item, 4)))
+
 
 if __name__ == '__main__':
     main()
